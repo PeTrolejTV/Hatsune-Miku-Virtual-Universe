d8c9e77c1744e85bb6fdce8d898a9adf4db564db
PeTrolejTV <69451085+PeTrolejTV@users.noreply.github.com>
2025-12-22 16:36:29 +0100

    Switch router history to use createWebHashHistory
    Changed router history mode to use hash for compatibility with GitHub Pages.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  // Hash história pre bezproblémové fungovanie na GitHub Pages
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+
+  ],
 })
 
 export default router
