cbadbaea3cb64df05f3cddd2bbf340199ee133bf
PeTrolejTV <69451085+PeTrolejTV@users.noreply.github.com>
2025-12-23 02:16:54 +0100

    Fix formatting and ensure consistent export in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  // Hash história pre bezproblémové fungovanie na GitHub Pages
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
+	// Hash história pre bezproblémové fungovanie na GitHub Pages
+	history: createWebHashHistory(import.meta.env.BASE_URL),
+	routes: [
 
-  ],
+	],
 })
 
 export default router
