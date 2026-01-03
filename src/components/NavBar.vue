5c4dadc4e672ac05c1e6714f9d6ec57aabe55a6e
PeTrolejTV <69451085+PeTrolejTV@users.noreply.github.com>
2026-01-03 02:44:26 +0100

    Refactor NavBar to use dynamic menu items

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,52 +1,19 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow">
 		<div class="container">
-			<RouterLink to="/" class="navbar-brand d-flex align-items-center">
+			<a href="/" class="navbar-brand d-flex align-items-center" @click.prevent="navigate('/')">
 				<i class="bi bi-music-note-list me-2 fs-4"></i>
 				<span class="fw-bold">Miku Universe</span>
-			</RouterLink>
-			<button
-				class="navbar-toggler"
-				type="button"
-				data-bs-toggle="collapse"
-				data-bs-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav ms-auto">
-					<li class="nav-item">
-						<RouterLink to="/" class="nav-link" active-class="active" @click="closeNavbar">
-							<i class="bi bi-house-door me-1"></i>Home
-						</RouterLink>
-					</li>
-					<li class="nav-item">
-						<RouterLink to="/gallery" class="nav-link" active-class="active" @click="closeNavbar">
-							<i class="bi bi-images me-1"></i>Gallery
-						</RouterLink>
-					</li>
-					<li class="nav-item">
-						<RouterLink to="/concerts" class="nav-link" active-class="active" @click="closeNavbar">
-							<i class="bi bi-calendar-event me-1"></i>Concerts
-						</RouterLink>
-					</li>
-					<li class="nav-item">
-						<RouterLink to="/merch" class="nav-link" active-class="active" @click="closeNavbar">
-							<i class="bi bi-shop me-1"></i>Merch
-						</RouterLink>
-					</li>
-					<li class="nav-item">
-						<RouterLink to="/wishlist" class="nav-link position-relative" active-class="active" @click="closeNavbar">
-							<i class="bi bi-heart me-1"></i>Wishlist
-						</RouterLink>
-					</li>
-					<li class="nav-item">
-						<RouterLink to="/cart" class="nav-link position-relative" active-class="active" @click="closeNavbar">
-							<i class="bi bi-cart me-1"></i>Cart
-						</RouterLink>
+					<li class="nav-item" v-for="item in menuItems" :key="item.path">
+						<a :href="item.path" class="nav-link" :class="{ active: $route.path === item.path }" @click.prevent="navigate(item.path)">
+							<i :class="['bi', item.icon, 'me-1']"></i>{{ item.name }}
+						</a>
 					</li>
 				</ul>
 			</div>
@@ -57,14 +24,32 @@
 <script>
 export default {
 	name: 'NavBar',
+	data() {
+		return {
+			menuItems: [
+				{ name: 'Home', path: '/', icon: 'bi-house-door' },
+				{ name: 'Gallery', path: '/gallery', icon: 'bi-images' },
+				{ name: 'Concerts', path: '/concerts', icon: 'bi-calendar-event' },
+				{ name: 'Merch', path: '/merch', icon: 'bi-shop' },
+				{ name: 'Wishlist', path: '/wishlist', icon: 'bi-heart' },
+				{ name: 'Cart', path: '/cart', icon: 'bi-cart' }
+			]
+		}
+	},
 	methods: {
+		navigate(path) {
+			if (this.$route.path === path) {
+				window.scrollTo({ top: 0, behavior: 'smooth' });
+			} else {
+				this.$router.push(path);
+			}
+			this.closeNavbar();
+		},
 		closeNavbar() {
-			const navbarCollapse = document.getElementById('navbarNav')
+			const navbarCollapse = document.getElementById('navbarNav');
 			if (navbarCollapse && navbarCollapse.classList.contains('show')) {
-				const toggleButton = document.querySelector('.navbar-toggler')
-				if (toggleButton) {
-					toggleButton.click()
-				}
+				const toggleButton = document.querySelector('.navbar-toggler');
+				if (toggleButton) toggleButton.click();
 			}
 		}
 	}
