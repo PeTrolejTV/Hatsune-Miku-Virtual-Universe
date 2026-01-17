b8974fbbf0546929ea1eb87b17db66cff411f7c5
PeTrolejTV <69451085+PeTrolejTV@users.noreply.github.com>
2026-01-17 23:22:33 +0100

    Create cart.js

---
+++ src/stores/cart.js
@@ -0,0 +1,50 @@
+import { defineStore } from 'pinia'
+
+export const useCartStore = defineStore('cart', {
+	state: () => ({
+		items: JSON.parse(localStorage.getItem('miku_cart_items') || '[]')
+	}),
+
+	getters: {
+		totalItems: (state) => state.items.reduce((acc, item) => acc + item.amount, 0),
+		totalPrice: (state) => state.items.reduce((acc, item) => acc + (item.amount * item.price), 0)
+	},
+
+	actions: {
+		addItem(item) {
+			const uniqueId = item.size ? `${item.type}-${item.id}-${item.size}` : `${item.type}-${item.id}`;
+			const existingItem = this.items.find(i => i.uniqueId === uniqueId);
+
+			if (existingItem) {
+				existingItem.amount += item.amount;
+			} else {
+				this.items.push({
+					...item,
+					uniqueId,
+					amount: item.amount
+				});
+			}
+			this.saveToLocalStorage();
+		},
+
+		removeItem(uniqueId) {
+			const index = this.items.findIndex(i => i.uniqueId === uniqueId);
+			if (index !== -1) {
+				const item = this.items[index];
+				this.items.splice(index, 1);
+				this.saveToLocalStorage();
+				return item;
+			}
+			return null;
+		},
+
+		clearCart() {
+			this.items = [];
+			this.saveToLocalStorage();
+		},
+
+		saveToLocalStorage() {
+			localStorage.setItem('miku_cart_items', JSON.stringify(this.items));
+		}
+	}
+})
