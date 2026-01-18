5f4d7246ea95eb2ca0541c442ecf695f9769e405
PeTrolejTV <69451085+PeTrolejTV@users.noreply.github.com>
2026-01-18 23:13:47 +0100

    Update NewsView.vue

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -11,32 +11,18 @@
 				</p>
 			</div>
 
-			<div v-if="!selectedArticle">
-				<NewsFilter 
-					:all-articles="articles" 
-					@update-results="setFilteredArticles"
-				/>
+			<NewsFilter @update-results="setFilteredArticles" />
 
-				<div v-if="displayArticles.length > 0" class="row g-4">
-					<div v-for="article in displayArticles" :key="article.id" class="col-md-6 col-lg-4">
-						<NewsCard 
-							:article="article"
-							@read-more="readFullArticle"
-						/>
-					</div>
-				</div>
-
-				<div v-else class="text-center py-5">
-					<i class="bi bi-search-heart display-1 text-muted opacity-25"></i>
-					<h3 class="mt-3 text-secondary">No news articles found.</h3>
+			<div v-if="displayArticles.length > 0" class="row g-4">
+				<div v-for="article in displayArticles" :key="article.id" class="col-md-6 col-lg-4">
+					<NewsCard :article="article" />
 				</div>
 			</div>
 
-			<NewsDetail 
-				v-else 
-				:article="selectedArticle" 
-				@back="closeArticle" 
-			/>
+			<div v-else class="text-center py-5">
+				<i class="bi bi-search-heart display-1 text-muted opacity-25"></i>
+				<h3 class="mt-3 text-secondary">No news articles found.</h3>
+			</div>
 		</div>
 	</div>
 </template>
@@ -44,60 +30,21 @@
 <script>
 import NewsCard from '@/components/NewsArticles/NewsCard.vue'
 import NewsFilter from '@/components/NewsArticles/NewsFilter.vue'
-import NewsDetail from '@/components/NewsArticles/NewsDetail.vue'
-import { newsData } from '@/data/news.js'
 
 export default {
 	name: 'NewsView',
 	components: {
 		NewsCard,
-		NewsFilter,
-		NewsDetail
+		NewsFilter
 	},
 	data() {
 		return {
-			articles: newsData,
-			displayArticles: [],
-			selectedArticle: null
-		}
-	},
-	watch: {
-		selectedArticle(newVal) {
-			if (newVal) {
-				document.title = `News - ${newVal.title} | Miku Universe`;
-			} else {
-				document.title = this.$route.meta.title || 'News | Miku Universe';
-			}
+			displayArticles: []
 		}
 	},
 	methods: {
 		setFilteredArticles(results) {
 			this.displayArticles = results;
-		},
-		readFullArticle(article) {
-			document.documentElement.style.scrollBehavior = 'auto';
-			this.selectedArticle = article;
-			
-			this.$nextTick(() => {
-				window.scrollTo(0, 0);
-				setTimeout(() => {
-					document.documentElement.style.scrollBehavior = '';
-				}, 50);
-			});
-		},
-		closeArticle() {
-			document.documentElement.style.scrollBehavior = 'auto';
-			this.selectedArticle = null;
-
-			this.$nextTick(() => {
-				const savedPos = sessionStorage.getItem('news-scroll-pos');
-				if (savedPos) {
-					window.scrollTo(0, parseInt(savedPos));
-				}
-				setTimeout(() => {
-					document.documentElement.style.scrollBehavior = '';
-				}, 50);
-			});
 		}
 	}
 }
